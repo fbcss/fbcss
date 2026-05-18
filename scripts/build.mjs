ac71d08ff5a15d9840ca2d3dc108c964f0965b77
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-05-18 15:25:09 -0400

    Fetch transcripts locally

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,8 @@
 import * as fs from "node:fs";
 import * as esbuild from "esbuild";
 
-const data = await fetch(
-  "https://raw.githubusercontent.com/fbcss/fbcss/main/data/transcripts.json",
-).then((r) => r.json());
-fs.writeFileSync("transcripts.js", `export const transcripts = ${JSON.stringify(data)};`);
+const data = fs.readFileSync("data/transcripts.json", "utf8");
+fs.writeFileSync("transcripts.js", `export const transcripts = ${data};`);
 
 await Promise.all([
   esbuild.build({
